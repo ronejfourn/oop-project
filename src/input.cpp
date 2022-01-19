82e1e8be6196579da6b4dff77503b07831ba6cf6
r1j4n <76547567+ronejfourn@users.noreply.github.com>
2022-01-20 01:11:11 +0545

    key/btn press works correctly and added btn bindings

--- src/input.cpp
+++ src/input.cpp
@@ -1,5 +1,9 @@
 #include "input.h"
 
+#define isDown 0x01
+#define isHeld 0x02
+#define wasPressed 0x04
+
 Input *Input::_instance = nullptr;
 
 Input *Input::GetInstance() {
@@ -8,9 +12,6 @@ Input *Input::GetInstance() {
 }
 
 Input::Input() {
-    _btnDown[0] = false;
-    _btnDown[1] = false;
-    _btnDown[2] = false;
     _btnBind[0] = nullptr;
     _btnBind[1] = nullptr;
     _btnBind[2] = nullptr;
@@ -18,49 +19,75 @@ Input::Input() {
 
 void Input::BindActionToKey(SDL_Scancode key, const std::function<void()>& action, bool hold) {
     _keyBind[key] = action;
-    _holdKey[key] = hold;
+    hold ? _keyDown[key].v |= isHeld : _keyDown[key].v &= ~isHeld;
+}
+
+void Input::BindActionToBtn(int btn, const std::function<void(int, int)>& action, bool hold) {
+    _btnBind[btn] = action;
+    hold ? _btnDown[btn].v |= isHeld : _btnDown[btn].v &= ~isHeld;
 }
 
 void Input::Handle() {
     for (auto &i : _keyBind) {
         SDL_Scancode key = i.first;
-        if (_keyDown[key]) {
-            _keyBind[key]();
-            _keyDown[key] = _holdKey[key];
+        if (_keyDown[key].v & isDown) {
+            i.second();
+            if (!(_keyDown[key].v & isHeld)) {
+                _keyDown[key].v |= wasPressed;
+                _keyDown[key].v &= ~isDown;
+            }
         }
     }
     for (int i = 0; i < 3; i ++) {
-        if (_btnDown[i] && _btnBind[i] != nullptr) {
-            _btnBind[i]();
-            _btnDown[i] = _holdBtn[i];
+        if ((_btnDown[i].v &isDown) && _btnBind[i] != nullptr) {
+            _btnBind[i](_clkx, _clky);
+            if (!(_btnDown[i].v & isHeld)) {
+                _btnDown[i].v |= wasPressed;
+                _btnDown[i].v &= ~isDown;
+            }
         }
     }
 }
 
 void Input::ClearBinds() {
     _keyBind.clear();
-    _holdKey.clear();
+    _keyDown.clear();
     _btnBind[0] = nullptr;
     _btnBind[1] = nullptr;
     _btnBind[2] = nullptr;
 }
 
 void Input::Flush() {
-    _keyDown.clear();
+    for (auto &i : _keyDown)
+        i.second.v &= isDown;
+    _btnDown[0].v &= isDown;
+    _btnDown[1].v &= isDown;
+    _btnDown[2].v &= isDown;
 }
 
 void Input::KeyUp(SDL_Scancode key) {
-    _keyDown[key] = false;
+    _keyDown[key].v &= ~wasPressed;
+    _keyDown[key].v &= ~isDown;
 }
 
 void Input::KeyDown(SDL_Scancode key) {
-    _keyDown[key] = true;
+    if (_keyDown[key].v & isHeld) {
+        _keyDown[key].v |= isDown;
+    } else if (!(_keyDown[key].v & wasPressed)){
+        _keyDown[key].v |= isDown;
+    }
 }
 
 void Input::BtnUp(int btn) {
-    _btnDown[btn - 1] = false;
+    _btnDown[btn - 1].v &= ~wasPressed;
+    _btnDown[btn - 1].v &= ~isDown;
 }
 
-void Input::BtnDown(int btn) {
-    _btnDown[btn - 1] = true;
+void Input::BtnDown(int btn, int x, int y) {
+    _clkx = x; _clky = y;
+    if (_btnDown[btn - 1].v & isHeld) {
+        _btnDown[btn - 1].v |= isDown;
+    } else if (!(_btnDown[btn - 1].v & wasPressed)){
+        _btnDown[btn - 1].v |= isDown;
+    }
 }
--- src/input.h
+++ src/input.h
@@ -6,18 +6,18 @@
 class Input {
     static Input *_instance;
 private:
-    std::map<SDL_Scancode, bool> _keyDown;
+    struct _st{ uint8_t v = 0; };
+    std::map<SDL_Scancode, _st> _keyDown;
     std::map<SDL_Scancode, std::function<void()>> _keyBind;
-    std::map<SDL_Scancode, bool> _holdKey;
-    bool _btnDown[3];
-    bool _holdBtn[3];
-    std::function<void()> _btnBind[3];
+    _st _btnDown[3];
+    int _clkx, _clky;
+    std::function<void(int, int)> _btnBind[3];
     Input();
 public:
     static Input *GetInstance();
 
     void BindActionToKey(SDL_Scancode key, const std::function<void()>& action, bool hold);
-    void BindActionToBtn(int btn, const std::function<void()>& action, bool hold);
+    void BindActionToBtn(int btn, const std::function<void(int, int)>& action, bool hold);
 
     void Handle();
     void Flush();
@@ -27,5 +27,5 @@ public:
     void KeyDown(SDL_Scancode key);
 
     void BtnUp  (int btn);
-    void BtnDown(int btn);
+    void BtnDown(int btn, int x, int y);
 };
